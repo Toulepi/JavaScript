7c321313bb0fe865a878f2039883ddbb7a2db79e
Toulepi <tchindafabrice@yahoo.fr>
2020-06-15 16:45:27 +0200

    commit 01-variables-et-types

---
+++ 01-variables-et-types/main.js
@@ -0,0 +1,18 @@
+        // DECLARATION DES VARIABLES
+
+let maVariable;   //déclaration d'une variable
+typeof maVariable;  // détermination du type de la variable avec typeof
+ maVariable = 25; //affectation ou assignation
+ console.log(typeof maVariable);  //affiche le type de la variable
+
+
+        // Javascript est un langage faiblement typé, en effet une variable peut prendre plusieurs types de valeurs (number, undefined, string, boolean)
+
+
+  maVariable = null;
+  console.log(typeof maVariable);  // null est de type "object"
+
+   // Utilisation des vaiables
+
+let a = 45, b = -12;
+console.log(a+b);
