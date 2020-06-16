f8734354d5c60bfd233d02a6a8fe8ad59077bc22
Toulepi <tchindafabrice@yahoo.fr>
2020-06-16 10:18:05 +0200

    commit 02-saisie-user

---
+++ 02-saisie-user/main.js
@@ -0,0 +1,25 @@
+// Affiche dans la console
+
+//console.log("test de connection");
+
+// pour afficher des informations
+//alert("Bonjour JS depuis Balessing");
+
+// boite de dialogue pour confirmation
+//let reponse=confirm("Voulez-vous vraiment supprimer votre produit ?")
+//console.log(reponse);
+
+// saisie utilisateur  --> Prompt
+//let Nom = prompt("Donner votre Nom");
+
+//console.log(Nom);
+let prenom=prompt("entrez votre prenom");
+let age=prompt("entrez votre age");
+
+//console.log('Samedi j\'irais chez JDC puis au "USA"');  //  '\' est appelé opérateur de protection
+// tout ce qui est saisie dans le prompt est considéré comme string
+
+`template de String`  //  `est appelé String Template
+console.log(`Tu es d'ores et déjà Développeur Web et mobile au "USA"`);
+console.log(`Bonjour ${prenom},\n vous avez ${age} ans`);  //  \n take to a new line
+
